c0091e8d51d46484a91a2dc4671b39416b51cb87
Svershko <andreycoks@gmail.com>
2024-05-08 23:25:10 +0300

    All reset table in db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-
-
         $this->call([
             RoleSeeder::class,
             CompanySeeder::class,
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/db/reset', function () {
+    DB::table('users_companies')->truncate();
+    DB::table('users_companies_roles')->truncate();
+    DB::table('invitations')->truncate();
+
     $seeder = new \Database\Seeders\DatabaseSeeder();
     $seeder->run();
     return 'success';
